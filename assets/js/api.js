192ff22c3341d1df275b4a7779e403ac90fdc253
RastelliBru <bruna.rastelli@gmail.com>
2024-03-12 09:25:32 -0300

    adicionando arquivos

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,5 @@
-
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/RastelliBru/portifolio/main/data/profile.json';
+    const fetching = await fetch(url)
+    return await fetching.json()
+}
